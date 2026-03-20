148063ffba41417f801af9c394f2ddc26fef61f5
MohammedAMizyed <mohammedamizayed2006@gmail.com>
2026-03-20 20:17:55 +0200

    testing

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -25,7 +25,7 @@ export default function Landing() {
           >
             {t("Hello I")}
 
-            <span className="block animate-pulse">Mohammed Dev</span>
+            <span className="block animate-pulse">Mohammed Developer</span>
           </h1>
           <h2
             data-aos="fade-up"
